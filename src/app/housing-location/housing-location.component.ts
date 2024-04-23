740877823e0ba409ee74cd9515ce29d0f56c2f66
nathangarciaf <nathangarfreitas@gmail.com>
2024-04-23 02:27:18 -0300

    new

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
     <p>
       housing-location works!
     </p>
   `,
-  styleUrl: './housing-location.component.css'
+  styleUrls: ['./housing-location.component.css'],
 })
+
 export class HousingLocationComponent {
+
+  @Input() housingLocation!: HousingLocation;
 
 }
